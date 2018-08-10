ab489228d632bd106c9ad6ed928f0a68f3e0873c
walli <wallypark710@gmail.com>
2018-08-10 23:11:28 +0900

    edit flatten indentation and write zip function

--- underbar.js
+++ underbar.js
@@ -5,28 +5,49 @@
 
 /////////////////////////////////////////////////////////////////////
     _.flatten = function(nestedArray, result) {
-	var result = [];
+		var result = [];
 
-	var convert = function( ele ) {
-	    if( typeof ele === 'number' ) {
-		result.push(ele);
-	    } else {
-		for( var i = 0; i < ele.length; i++ ) {
-		convert( ele[i] );
-		}
-	    }
-	}	
+		var convert = function( ele ) {
+		    if( typeof ele === 'number' ) {
+			result.push(ele);
+		    } else {
+			for( var i = 0; i < ele.length; i++ ) {
+			convert( ele[i] );
+			}
+		    }
+		}	
 
-	for( var i = 0; i < nestedArray.length; i++ ) {
-	    convert(nestedArray[i]);
-	}	
+		for( var i = 0; i < nestedArray.length; i++ ) {
+		    convert(nestedArray[i]);
+		}	
 
-	return result;
+		return result;
     };
 
 //////////////////////////////////////////////////////////////////////
 
+	_.zip = function() {
+	    var result = [];
+	    var temp = [];
+	    var key = Array.prototype.slice.call(arguments);
+	    var maxLengthArray = _.reduce(key, function(acc, ele){
+	      if( acc.length > ele.length ) {
+	        return acc;
+	      } else {
+	        return ele;
+	      }
+	    });
+	    for( var i = 0; i < maxLengthArray.length; i++ ) {
+	      temp = [];
+	      for( var j = 0; j < key.length; j++ ) {
+	        temp.push(key[j][i]);
+	      }
+	      result.push(temp);
+	    }
 
+	    return result;
+
+  	};
 
 
 
